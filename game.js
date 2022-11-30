e8d8be38ada531d7a1939477cf480d57486a85be
ronald <89151207+ronaldhsp@users.noreply.github.com>
2022-11-30 19:41:08 +0800

    Implement function to determine winner

--- game.js
+++ game.js
@@ -9,6 +9,17 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   // return a string that declares the winner in the format "You lose/win! ... beats ..."
+  let playerWins = (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper");
+
+  if (playerWins) {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection === computerSelection) {
+    return "Draw!";
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  }
 }
 
 function game() {
