ae9339ea701cc1c3702512df03410a10a3f645cd
ronald <89151207+ronaldhsp@users.noreply.github.com>
2022-11-30 19:28:37 +0800

    Add unimplemented functions required for game

--- game.js
+++ game.js
@@ -1,3 +1,13 @@
 "use strict";
 
-console.log("hey");
+function getComputerChoice() {
+  // randomly return rock, paper or scissors
+}
+
+function playRound(playerSelection, computerSelection) {
+  // return a string that declares the winner in the format "You lose/win! ... beats ..."
+}
+
+function game() {
+  // call play round for a 5 round game
+}
